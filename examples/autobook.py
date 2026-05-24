88eed7d5fc80da036ecb444e83879eee5fde9f99
jacktwhiston <65812393+jacktwhiston@users.noreply.github.com>
2026-05-25 00:42:22 +1000

    Update autobook.py
    Attempt 2 fix time issue

--- examples/autobook.py
+++ examples/autobook.py
@@ -1,18 +1,19 @@
 import datetime
-
+import zoneinfo
 from htg import CondecoBooker
 
 
 def main():
     booker = CondecoBooker()
+    aest = zoneinfo.ZoneInfo("Australia/Brisbane")
 
     # Let's book the week a fortnight from now
-    now = datetime.datetime.now()
-    days_to_monday = (7 - now.weekday()) % 7
-    monday_fortnight = now + datetime.timedelta(days=days_to_monday + 7)
-
+    today = datetime.datetime.now(tz=aest).date()
+    days_until_monday = (7 - today.weekday()) % 7 or 7
+    target_monday = today + datetime.timedelta(days=days_until_monday + 7)
+    
     for i in range(5):
-        booker.book_desk(monday_fortnight + datetime.timedelta(days=i))
+        booker.book_desk(datetime.datetime.combine(target_monday + datetime.timedelta(days=i), datetime.time()))
 
 
 if __name__ == '__main__':
