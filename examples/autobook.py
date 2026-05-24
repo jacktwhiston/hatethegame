681189d09d8e264ef7364eecba056b738e442c3e
jacktwhiston <65812393+jacktwhiston@users.noreply.github.com>
2026-05-25 00:35:38 +1000

    Update autobook.py
    Fixed day calculation book

--- examples/autobook.py
+++ examples/autobook.py
@@ -8,8 +8,8 @@ def main():
 
     # Let's book the week a fortnight from now
     now = datetime.datetime.now()
-    delta_days = 14 - now.weekday()
-    monday_fortnight = now + datetime.timedelta(days=delta_days)
+    days_to_monday = (7 - now.weekday()) % 7
+    monday_fortnight = now + datetime.timedelta(days=days_to_monday + 7)
 
     for i in range(5):
         booker.book_desk(monday_fortnight + datetime.timedelta(days=i))
